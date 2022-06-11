785153fe10710b0f8317bed9ec99465bf656d275
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-11 14:56:00 +0100

    rotate functions passed tests

--- components/rotateRover.js
+++ components/rotateRover.js
@@ -2,14 +2,41 @@
 //     <- N <-
 //     W     E
 //     -> S ->
-
-function rotateRoverToLeft(rover) {}
+function rotateRoverToLeft(rover) {
+  if (rover.orientation === "N") {
+    rover.orientation = "W";
+    return `Rover current position: { X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  } else if (rover.orientation === "W") {
+    rover.orientation = "S";
+    return `Rover current position: { X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  } else if (rover.orientation === "S") {
+    rover.orientation = "E";
+    return `Rover current position: { X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  } else if (rover.orientation === "E") {
+    rover.orientation = "N";
+    return `Rover current position: { X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  }
+}
 
 // rotate to right
 //     -> N ->
 //     W     E
 //     <- S <-
-function rotateRoverToRight(rover) {}
+function rotateRoverToRight(rover) {
+  if (rover.orientation === "N") {
+    rover.orientation = "E";
+    return `Rover current position: {X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  } else if (rover.orientation === "E") {
+    rover.orientation = "S";
+    return `Rover current position: {X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  } else if (rover.orientation === "S") {
+    rover.orientation = "W";
+    return `Rover current position: {X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  } else if (rover.orientation === "W") {
+    rover.orientation = "N";
+    return `Rover current position: {X:${rover.position.x} Y:${rover.position.y}, Direction:${rover.orientation} }`;
+  }
+}
 
 module.exports = {
   rotateRoverToRight,
