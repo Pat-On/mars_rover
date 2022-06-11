02a8b5a07d1a88754cf7390a371aaad6dbd8df03
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-11 14:38:02 +0100

    rotate right tests

--- __tests__/rotateRover.test.js
+++ __tests__/rotateRover.test.js
@@ -4,6 +4,21 @@ const {
 } = require("../components/rotateRover");
 const MarsRover = require("../components/marsRover");
 
-describe("MarsRover rotationing an orientation to right", () => {});
+describe("MarsRover rotationing an orientation to right", () => {
+  it.each`
+    fromDirection | toDirection
+    ${"N"}        | ${"E"}
+    ${"E"}        | ${"S"}
+    ${"S"}        | ${"W"}
+    ${"W"}        | ${"N"}
+  `(
+    "Rotating Rover from direction to $fromDirection to direction to $toDirection",
+    ({ fromDirection, toDirection }) => {
+      const rover2022 = new MarsRover(0, 5, fromDirection);
+      rotateRoverToRight(rover2022);
+      expect(rover2022.orientation).toBe(toDirection);
+    }
+  );
+});
 
 describe("MarsRover rotationing an orientation to left", () => {});
