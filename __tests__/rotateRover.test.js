b5ef313331b2b6936e8f528227df48a7ff4ae365
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-11 14:43:08 +0100

    it.each tests - rotate rover to left

--- __tests__/rotateRover.test.js
+++ __tests__/rotateRover.test.js
@@ -21,4 +21,19 @@ describe("MarsRover rotationing an orientation to right", () => {
   );
 });
 
-describe("MarsRover rotationing an orientation to left", () => {});
+describe("MarsRover rotationing an orientation to left", () => {
+  it.each`
+    fromDirection | toDirection
+    ${"N"}        | ${"W"}
+    ${"W"}        | ${"S"}
+    ${"S"}        | ${"E"}
+    ${"E"}        | ${"N"}
+  `(
+    "Rotating Rover from direction to $fromDirection to direction to $toDirection",
+    ({ fromDirection, toDirection }) => {
+      const rover2022 = new MarsRover(0, 5, fromDirection);
+      rotateRoverToLeft(rover2022);
+      expect(rover2022.orientation).toBe(toDirection);
+    }
+  );
+});
